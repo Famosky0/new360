80a2b19bf204d1516fa4df854c455a7c78d9ab3f
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 21:26:07 +0100

    Update StepOne.tsx

--- src/app/dashboard/components/Bookings/StepOne.tsx
+++ src/app/dashboard/components/Bookings/StepOne.tsx
@@ -1,45 +1,20 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { bookingSchema, profileSchema } from "../Interface";
-import { retrieveProfile } from "@/services/request";
+import React, { useState } from "react";
+import { bookingSchema } from "../Interface";
 
 const BookingProcessOne = ({
   setBookingInfo,
   bookingInfo,
-  profile,
-  setProfile,
 }: {
   setBookingInfo: React.Dispatch<React.SetStateAction<bookingSchema>>;
   bookingInfo: bookingSchema;
-  profile: profileSchema;
-  setProfile: React.Dispatch<React.SetStateAction<profileSchema>>;
 }) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const name = e.target.name;
-    const value = e.target.value;
+  const handleChange = (e: any) => {
+    let name = e.target.name;
+    let value = e.target.value;
     setBookingInfo({ ...bookingInfo, [name]: value });
   };
-
-  const getUserProfile = async () => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      const data = await retrieveProfile(accessToken);
-      if (data) {
-        setProfile(data);
-      }
-    }
-  };
-
-  useEffect(() => {
-    getUserProfile();
-  }, []);
-
-  useEffect(() => {
-    // Automatically set the plan to JASPER on component mount
-    setBookingInfo({ ...bookingInfo, plan: "JASPER" });
-  }, []);
-
   return (
     <div className="w-full flex flex-col gap-4">
       <div className="w-full flex flex-col gap-2">
@@ -53,56 +28,53 @@ const BookingProcessOne = ({
             type="text"
             id="full_name"
             name="full_name"
-            value={profile.first_name + " " + profile.last_name}
-            disabled
+            placeholder="Enter your Full Name"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="Phone_number">WhatsApp Number</label>
+          <label htmlFor="Phone_number">Phone Number</label>
           <input
             type="number"
             id="Phone_number"
             name="phone"
-            value={bookingInfo.phone}
+            value={bookingInfo["phone"]}
             onChange={handleChange}
             placeholder="+2348149055068"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
+
         <div>
-          <label htmlFor="plan">Plan</label>
-          <input
-            type="text"
-            id="plan"
-            name="plan"
-            value="JASPER"  // Set the plan name to JASPER
-            disabled={true}  // Disable the input to prevent user changes
-            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
-          />
-        </div>
-        <div>
-          <label htmlFor="date">Date</label>
+          <label htmlFor="password">Date</label>
           <input
             type="date"
             id="date"
             name="shooting_date"
-            value={bookingInfo.shooting_date}
+            value={bookingInfo["shooting_date"]}
             onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="time">Time (between 08:00 and 19:00)</label>
+          <label htmlFor="time">Time </label>
           <input
             type="time"
             id="time"
             name="shooting_time"
-            value={bookingInfo.shooting_time}
+            value={bookingInfo["shooting_time"]}
             onChange={handleChange}
+            placeholder={new Date().getTime.toString()}
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
+
+        {/* <div className="w-full flex flex-col gap-2.5">
+          <button className="w-full min-h-12 bg-primary rounded-md mt-3">
+            Make payment
+          </button>
+        </div> */}
       </form>
     </div>
   );
