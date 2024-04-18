08e6b83c5891c66a3de34779a78436b3a20d47a4
Famosky0 <135935571+Famosky0@users.noreply.github.com>
2024-04-18 21:15:51 +0100

    Update StepOne.tsx

--- src/app/dashboard/components/Bookings/StepOne.tsx
+++ src/app/dashboard/components/Bookings/StepOne.tsx
@@ -35,14 +35,10 @@ const BookingProcessOne = ({
     getUserProfile();
   }, []);
 
-  // Helper function to format the date in yyyy-MM-dd format
-  const formattedTodayDate = () => {
-    const today = new Date();
-    const day = `0${today.getDate()}`.slice(-2);
-    const month = `0${today.getMonth() + 1}`.slice(-2);
-    const year = today.getFullYear();
-    return `${year}-${month}-${day}`;
-  };
+  useEffect(() => {
+    // Automatically set the plan to JASPER on component mount
+    setBookingInfo({ ...bookingInfo, plan: "JASPER" });
+  }, []);
 
   return (
     <div className="w-full flex flex-col gap-4">
@@ -57,7 +53,7 @@ const BookingProcessOne = ({
             type="text"
             id="full_name"
             name="full_name"
-            value={`${profile.first_name} ${profile.last_name}`}
+            value={profile.first_name + " " + profile.last_name}
             disabled
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
@@ -65,38 +61,45 @@ const BookingProcessOne = ({
         <div>
           <label htmlFor="Phone_number">WhatsApp Number</label>
           <input
-            type="tel" // Changed type from number to tel for better phone number handling
+            type="number"
             id="Phone_number"
             name="phone"
-            value={bookingInfo["phone"]}
+            value={bookingInfo.phone}
             onChange={handleChange}
             placeholder="+2348149055068"
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
-
+        <div>
+          <label htmlFor="plan">Plan</label>
+          <input
+            type="text"
+            id="plan"
+            name="plan"
+            value="JASPER"  // Set the plan name to JASPER
+            disabled={true}  // Disable the input to prevent user changes
+            className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
+          />
+        </div>
         <div>
           <label htmlFor="date">Date</label>
           <input
             type="date"
             id="date"
             name="shooting_date"
-            value={bookingInfo["shooting_date"]}
+            value={bookingInfo.shooting_date}
             onChange={handleChange}
-            min={formattedTodayDate()} // Set minimum date to today's date
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
         <div>
-          <label htmlFor="time">Time (between 08:00 and 23:00)</label>
+          <label htmlFor="time">Time (between 08:00 and 19:00)</label>
           <input
             type="time"
             id="time"
             name="shooting_time"
-            value={bookingInfo["shooting_time"]}
+            value={bookingInfo.shooting_time}
             onChange={handleChange}
-            min="08:00" // Set minimum time to 08:00
-            max="23:00" // Set maximum time to 23:00
             className="w-full bg-white rounded-md min-h-12 mt-1.5 p-2 text-black"
           />
         </div>
